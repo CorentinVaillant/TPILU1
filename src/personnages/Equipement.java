46a6b870208d2aaf8c7eea116206ee55a808394f
CorentinVaillant <contact.corentin.vaillant@gmail.com>
2024-09-23 18:51:43 +0200

    les énumérés

---
+++ src/personnages/Equipement.java
@@ -0,0 +1,16 @@
+package personnages;
+
+public enum Equipement {
+	CASQUE("casque"),
+	BOUCLIER("bouclier");
+	
+	private String nom;
+
+	private Equipement(String nom) {
+		this.nom = nom;
+	}
+	
+	public String toString() {
+		return this.nom;
+	}
+}
