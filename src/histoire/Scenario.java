2f2ab0851018ae9bc3582529103c47d905af3b5f
CorentinVaillant <contact.corentin.vaillant@gmail.com>
2024-09-23 18:16:40 +0200

    le village gaulois

--- src/histoire/Scenario.java
+++ src/histoire/Scenario.java
@@ -1,12 +1,46 @@
 package histoire;
 
+import personnages.Chef;
 import personnages.Druide;
 import personnages.Gaulois;
 import personnages.Romain;
+import personnages.Village;
 
 public class Scenario {
 
 	public static void main(String[] args) {
+		//histoire1();
+		
+		Village dieppe = new Village("Village des Irréductibles", 30);
+		//Gaulois gaulois = dieppe.trouverHabitant(30); 
+		/*	Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 30 out of bounds for length 30
+			at personnages.Village.trouverHabitant(Village.java:26)
+			at histoire.Scenario.main(Scenario.java:14)
+			
+			On demande un villageois en dehors du tableau, on cherche le 31 ème éléments d'un tableau de 30 éléments
+		*/
+		
+		Chef abraracourcix = new Chef("Abraracourcix", 6, dieppe);
+		dieppe.setChef(abraracourcix);
+		
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		dieppe.ajouterHabitant(asterix);
+		
+		//Gaulois gaulois = dieppe.trouverHabitant(1);
+		//System.out.println(gaulois);
+		/*
+		 	Cela renvoie "null", car la case 1 du tableau n'est pas initialiser avec un villageois
+		  
+		 */
+		
+		Gaulois obelix = new Gaulois("Obélix", 25);
+		dieppe.ajouterHabitant(obelix);
+		
+		dieppe.afficherVillageois();
+		
+	}
+	
+	private static void histoire1() {
 		Gaulois asterix = new Gaulois("Astérix", 8);
 		Gaulois obelix = new Gaulois("Obélix",2147483647);
 		
@@ -26,8 +60,6 @@ public class Scenario {
 		
 		minus.parler("UN GAU... UN GAUGAU...");
 		asterix.frapper(minus);
-	
-
 	}
 
 }
--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -1,13 +1,51 @@
 package personnages;
 
+import myUtil.Util;
+
 public class Village {
-	private String nom;
 
-	public Village(String nom) {
-		this.nom = nom;
-	}
+    private String nom;
+    private Chef chef;
+    private Gaulois[] villageois;
+    private int nbVillageois = 0;
 
-	public String getNom() {
-		return nom;
-	}
+    public Village(String nom, int nbVillageoisMax) {
+        this.nom = nom;
+        this.villageois = new Gaulois[nbVillageoisMax];
+    }
+    
+    public void setChef(Chef chef) {
+        this.chef = chef;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+    
+    public boolean ajouterHabitant(Gaulois habitant) {
+    	if (nbVillageois >= villageois.length) {
+    		System.out.println("Par Toutatis ! Nous somme bien trop nombreux, nous ne pouvons pas t'accepter *sob* *sob*");
+    		return false;
+    	}
+    	
+    	villageois[nbVillageois++] = habitant;
+    	return true;
+    }
+    
+    public Gaulois trouverHabitant(int numeroHutte) {
+    	return this.villageois[numeroHutte];
+    }
+
+
+    public void afficherVillageois() {
+    	Util.println("Dans le village du chef " + chef.getNom() + " vivent les légendaires gaulois :");
+    	for(int i = 0; i<this.nbVillageois;i++) {
+    		if (villageois[i] != null) {
+    			Util.println("-"+villageois[i].getNom());
+    		}
+    	}
+    	
+    	
+    }
+    
 }
