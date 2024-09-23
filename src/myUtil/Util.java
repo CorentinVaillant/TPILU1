602a70c429b445177a184d344e579bbfdb9259b6
CorentinVaillant <contact.corentin.vaillant@gmail.com>
2024-09-23 18:17:06 +0200

    le village gaulois

---
+++ src/myUtil/Util.java
@@ -0,0 +1,11 @@
+package myUtil;
+
+public class Util {
+	public static <T> void println(T writen){
+		System.out.println(writen);
+	}
+
+	public static <T> void print(T writen){
+		System.out.print(writen);
+	}
+}
